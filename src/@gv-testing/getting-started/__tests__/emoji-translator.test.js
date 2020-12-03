a64f4567826a9073aeefa6b713725005e51f721b
Tomas <tomas.hrobarik@guidevision.cz>
2020-12-03 14:22:35 +0100

    test: calculator and emoji

--- src/@gv-testing/getting-started/__tests__/emoji-translator.test.js
+++ src/@gv-testing/getting-started/__tests__/emoji-translator.test.js
@@ -3,5 +3,11 @@ const { emojiTranslator, emojiTranslatorV2 } = require("../emoji-translator");
 describe('Test Emoji Translator', () => {
 
 	test('should replace :)', () => {
+		expect(emojiTranslator(':)')).toBe('😊');
 	});
+
+	test('should replace Hello :)', () => {
+		expect(emojiTranslator('Hello :)')).toBe('Hello 😊');
+	});
+
 });
